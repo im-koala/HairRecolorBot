881273b1d4e5bd7175d2025795e7a4a5c40da008
Just Koala <77019649+im-koala@users.noreply.github.com>
2022-03-06 18:39:38 +0300

    ✏️ Fix typo

--- app/handlers/private/start.py
+++ app/handlers/private/start.py
@@ -5,7 +5,7 @@ from aiogram import html
 async def get_start_message(m: Message):
     await m.answer_sticker("CAACAgIAAxkBAAENnrxh7UpI-RgEmNkUhvUqV27L5-cZiQAChwIAAladvQpC7XQrQFfQkCME")
     await m.answer(
-        f'Привет =) Этот поможет тебе в пару кликов поменять цвет своих волос на фото! Просто отправь мне свою фоточку\nP.S чуть больше о боте можешь узнать в /help{html.link("&#8288;", "https://telegra.ph/file/1126ea02a1caccdd8480f.jpg")}', disable_web_page_preview=False,
+        f'Привет =) Этот бот поможет тебе в пару кликов поменять цвет своих волос на фото! Просто отправь мне свою фоточку\nP.S чуть больше о боте можешь узнать в /help{html.link("&#8288;", "https://telegra.ph/file/1126ea02a1caccdd8480f.jpg")}', disable_web_page_preview=False,
     )
 
 
